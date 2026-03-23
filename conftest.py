84954715b393544ebe5b092069d83eaff170fbb0
Arvi <your@email.com>
2026-03-23 17:06:17 +0530

    updated_workflows

--- conftest.py
+++ conftest.py
@@ -1,11 +1,16 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import pytest
 import time
 
 @pytest.fixture
 def setup():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # IMPORTANT
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
 
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(options=chrome_options)
     driver.maximize_window()
     driver.implicitly_wait(10)
 
